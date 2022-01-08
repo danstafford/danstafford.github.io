81ea2d14b1f89362e5e5216a742b90aaf177c6bb
danstafford <danstafford@users.noreply.github.com>
2022-01-08 14:40:47 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 // alert('hi');
 
-fetch("http://dummy.restapiexample.com/api/v1/employees")
+fetch("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
 .then( (response) => response.json())
 .then((data)=> console.log(data))// output will be the required data
 .catch( (error) => console.log(error))
