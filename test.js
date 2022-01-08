c9160ec242f0e5a38c30a1cf6aa7385247b2e858
danstafford <danstafford@users.noreply.github.com>
2022-01-08 14:37:27 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 alert('hi');
+
+fetch("http://dummy.restapiexample.com/api/v1/employees")
+.then( (response) => response.json())
+.then((data)=> console.log(data))// output will be the required data
+.catch( (error) => console.log(error))
+
+
+
+document.getElementById("demo").innerHTML = data;
