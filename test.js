d924bf2d62e7abb7fc89c7efe0cb5c47874f5eb9
danstafford <danstafford@users.noreply.github.com>
2022-01-08 15:17:59 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,28 @@
 // alert('hi');
 
-fetch("https://api.openbrewerydb.org/breweries?by_state=massachusetts&per_page=3")
-.then( (response) => response.json())
-.then((data)=> console.log(data))// output will be the required data
-.catch( (error) => console.log(error))
-
 var demo = document.getElementById("demo");
 
-const result = JSON.parse(data);
 
-for (const key in result){
-  if(obj.hasOwnProperty(key)){
-    console.log(`${key} : ${res[key]}`);
-    demo.innerHTML += `${key} : ${res[key]}` + "<br>";
-  }
-}
+fetch("https://api.openbrewerydb.org/breweries?by_state=massachusetts&per_page=3")
+.then( (response) => response.json())
+.then((data)=> {
+      let breweries = data;
+      breweries.map(function(brewery) {
+      demo.innerHTML = `${brewery.name}`;
+      })
+.catch( (error) => console.log(error))
+
+
+  
+  
+  
+  
+// const result = JSON.parse(data);
+
+// for (const key in result){
+//   if(obj.hasOwnProperty(key)){
+//     console.log(`${key} : ${res[key]}`);
+//     demo.innerHTML += `${key} : ${res[key]}` + "<br>";
+//   }
+// }
 
